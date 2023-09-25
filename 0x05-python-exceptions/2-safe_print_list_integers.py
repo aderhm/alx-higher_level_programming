cabd45528a0c980e5e5a78064f329f2a8f7d3310
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-09-25 21:08:28 +0100

    Safe printing of a list of integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     cntr = 0
-    for i in my_list:
-        if cntr < x:
-            try:
-                print("{:d}".format(i), end='')
-                cntr += 1
-            except (ValueError, TypeError):
-                pass
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            cntr += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
     print()
     return (cntr)
