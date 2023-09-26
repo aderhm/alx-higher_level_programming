b9708533236080419aa208740eee6bdd82a372f4
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-09-26 23:28:21 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,12 @@
 class Square:
     """Represents a square."""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -22,7 +22,7 @@ class Square:
 
     @property
     def position(self):
-        return self.__size
+        return (self.__position)
 
     @position.setter
     def position(self, value):
@@ -45,4 +45,4 @@ class Square:
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            print()
