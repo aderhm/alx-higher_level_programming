82618fcca37498995548466514df7942643ab258
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-01 11:45:46 +0100

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,7 @@ def matrix_divided(matrix, div):
         TypeError: If the matrix contains non-numbers.
         TypeError: If the matrix contains rows of different sizes.
         TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
+        ZeroDivisionError: If div == 0.
     Returns:
         A new matrix representing the result of the division.
     """
