b04aed16c196021a03c1e73fd4854ad596031a1d
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-09 22:36:25 +0100

    Raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents a class: BaseGeometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
