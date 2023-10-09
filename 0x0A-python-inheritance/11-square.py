c1951bd341b3794850e9dced4dcd4c07a9b88158
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-09 23:16:55 +0100

    Add __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a class: Square."""
+
+    def __init__(self, size):
+        """ Initializes """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Returns sqaure area"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """Prints the object"""
+        return ("[{}] {}/{}".format(type(self).__name__, self.__size,
+                                    self.__size))
