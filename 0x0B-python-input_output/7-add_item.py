a99984602bb7d2dd19e434dcfb017bdb755af261
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-10 22:11:59 +0100

    Add item script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,14 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-list = []
-
-for elem in sys.argv:
-    list += elem
-
-save_to_json_file(list, filename)
-
-load_from_json_file(filename)
+    try:
+        lst = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        lst = []
+    lst.extend(sys.argv[1:])
+    save_to_json_file(lst, "add_item.json")
