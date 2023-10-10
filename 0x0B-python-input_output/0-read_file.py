a9b57ddba9f11afb688b208fc6e415e05bf37563
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-10 20:37:51 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Defines a function."""
+
 
 def read_file(filename=""):
+    """Reads a file and prints its content.
+    Args:
+        filename: the file to read.
+    """
+
     with open(filename, "r", encoding="utf-8") as myFile:
         print(myFile.read())
