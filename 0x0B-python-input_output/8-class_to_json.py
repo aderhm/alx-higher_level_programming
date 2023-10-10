00eb080e65da99fc8d227ae4bd9f378254768f6a
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-10 22:18:05 +0100

    Get the dict representation

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function."""
+
+
+def class_to_json(obj):
+    """Returns the dictionary represntation
+        of a simple data structure.
+    """
+    return (obj.__dict__)
