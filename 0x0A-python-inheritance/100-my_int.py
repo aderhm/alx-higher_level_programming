49aeffc7f66001e2ac530040ba9d611a21d449b7
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-12 00:33:08 +0100

    Inherit from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle."""
+
+
+class MyInt(int):
+    """Represents a class: MyInt."""
+
+    def __eq__(self, other):
+        """Inverting the == operator"""
+        return (int.__eq__(self, other))
+
+    def __ne__(self, other):
+        """Inverting the != operator"""
+        return (int.__ne__(self, other))
