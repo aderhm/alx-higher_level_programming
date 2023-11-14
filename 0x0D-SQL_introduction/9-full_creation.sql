0bcbf3b13d6593bbfc20e4b79f2feae55a895fa8
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-11-14 21:21:53 +0100

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE DATABASE IF NOT EXISTS (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INT DATABASE VALUES (1, "John", 10);
+INSERT INT DATABASE VALUES (2, "Alex", 3);
+INSERT INT DATABASE VALUES (3, "Bob", 14);
+INSERT INT DATABASE VALUES (4, "George", 8);
