b96bbdd166136a32d674341985e4c6ed9b28a7dc
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-11-15 23:41:12 +0100

    List Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') as tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
-ORDER VY tv_shows.title, tv_show_genres.genre_id ASC;
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
