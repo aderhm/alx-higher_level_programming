7dcb85b960661974c39525a6ae245113a972acb4
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-13 08:32:59 +0100

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,8 +10,9 @@ module.exports = class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        console.log('X');
+        process.stdout.write('X');
       }
+      console.log();
     }
   }
 };
