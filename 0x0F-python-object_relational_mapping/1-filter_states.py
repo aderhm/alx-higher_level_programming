6e3d12d974825e4c5a18f41920f2a422b526a1ce
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-15 11:43:39 +0100

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
+"""This script lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
     db = MySQLdb.connect(
-        port=3306,
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-
     cur = db.cursor()
     qr = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id"
-    try:
-        cur.execute(qr)
-        rows = cur.fetchall()
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: %s" % str(e))
+    cur.execute(qr)
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
