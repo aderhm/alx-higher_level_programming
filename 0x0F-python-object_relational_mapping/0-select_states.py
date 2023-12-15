93e198faa20c8dcea08e0cc8c80c242e96b6d21a
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-15 09:03:35 +0100

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost",
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+    )
+
+cur = db.cursor()
+try:
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+except MySQLdb.Error as e:
+    try:
+        print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+    except IndexError:
+        print("MySQL Error: %s" % str(e))
+for row in rows:
+    print(row)
+cur.close()
