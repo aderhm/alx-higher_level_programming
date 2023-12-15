2db492bc155993feecde4282e602a27c4811450a
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-15 11:49:14 +0100

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This script lists all states from the database hbtn_0e_0_usa"""
+"""This script lists all states starting with N
+from the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
@@ -12,7 +14,11 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cur = db.cursor()
-    qr = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id"
+    qr = " ".join([
+        "SELECT * FROM states",
+        "WHERE states.name LIKE BINARY 'N%'",
+        "ORDER BY states.id"
+    ])
     cur.execute(qr)
     rows = cur.fetchall()
     for row in rows:
