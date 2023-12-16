96f4c8625ce428adaf78cfb81fd076fd24928618
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-16 15:12:13 +0100

    List states that contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""""""
+"""This script lists all state that contains 'a'
+in the db via SQLAlchmey"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
