b4d8bbc881eb196a4154d047e9152468fa1e01a6
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-05 18:07:14 +0100

    Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message.
-curl -sX PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sX PUT -L -H "content-type:text/plain" -d "user_id=98" 0.0.0.0:5000/catch_me
