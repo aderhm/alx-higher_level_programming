97ff414b120537b0ab614ed94c65bf4564dcde22
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-07 20:34:13 +0100

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,15 +4,13 @@ and displays the body of the response (decoded in utf-8).
 """
 
 import sys
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 from urllib.error import HTTPError
 
 
 if __name__ == '__main__':
-    req = Request(sys.argv[1])
     try:
-        with urlopen(req) as res:
-            body = res.read()
+        with urlopen(sys.argv[1]) as res:
+            print(res.read().decode('utf-8'))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
-    print(body.decode('utf-8'))
