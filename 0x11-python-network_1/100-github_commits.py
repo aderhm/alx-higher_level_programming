49ae920579fb0dc4cc07fa72902b9d70c21bca20
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-07 22:02:19 +0100

    List 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script lists 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    res = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+    commits = res.json()
+    for i in range(10):
+        print("{}: {}".format(
+            commits[i].get('sha'),
+            commits[i].get('commit').get('author').get('name')
+        ))
