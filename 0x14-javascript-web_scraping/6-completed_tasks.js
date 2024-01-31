e8cb30b8a3f3cbeb90ab9a65f74c8d9ada9ad3b3
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-31 22:23:12 +0100

    Print how many complated

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (!error) {
+    const resData = JSON.parse(body);
+    for (let i = 0; i < resData.length; i++) {
+      if (resData[i].completed) {
+        console.log(resData[i]);
+      }
+    }
+  }
+});
