cc0022e294682770f7b219861553455d6aa346d2
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-31 23:13:24 +0100

    Find who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (!error) {
     const resData = JSON.parse(body);
