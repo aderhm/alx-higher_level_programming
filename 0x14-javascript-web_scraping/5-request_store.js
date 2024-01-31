1b4095dcbab3e017126771218f359e3dd13e1f9e
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-31 22:12:17 +0100

    Write the response to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,6 +3,6 @@ const request = require('request');
 const fs = require('fs');
 request(process.argv[2], (error, response, body) => {
   if (!error) {
-    fs.writeFileSync(process.argv[3], body, 'utif-8');
+    fs.writeFileSync(process.argv[3], body, 'utf-8');
   }
 });
